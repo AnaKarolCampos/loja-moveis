fb646416a1ab2e15eab1c19e58a6f3ec0be3ad6d
AnaKarolCampos <camposanakarol@gmail.com>
2024-12-09 18:13:37 -0300

    criação models e views

--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Home(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=200)
+    categoria = models.TextField(max_length=50)
+    preco = models.DecimalField(decimal_places=2, max_digits=8)
+    codigo = models.CharField(max_length=5)
+    estoque = models.BooleanField(default=True) # colocar quantidade ao invés de 
+
+    def __str__(self):
+        return self.nome
