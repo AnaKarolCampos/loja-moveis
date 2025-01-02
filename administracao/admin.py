a69981d218c451311c818d514ab3c95d5db36cdf
Gustavo038 <gus.gp754@gmail.com>
2025-01-02 15:22:46 -0300

    criação moldels administração

--- administracao/admin.py
+++ administracao/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from.models import Produtos,Cliente,Vendas
+
+admin.site.register (Produtos)
+admin.site.register (Cliente)
+admin.site.register (Vendas)
--- administracao/models.py
+++ administracao/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+class Produtos(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.PositiveIntegerField()
+    categoria = models.CharField(max_length=100)
+
+    def str(self):
+        return self.nome
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=15)
+    endereco = models.TextField()
+
+    def str(self):
+        return self.nome
+
+class Vendas(models.Model):
+    descricao = models.CharField(max_length=255)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    data_venda = models.DateField()
+    
+
+    def str(self):
+        return self.descricao
+
 # Create your models here.
