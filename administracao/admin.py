d7863fdd1a1756b67388b6f7dd6cd00ac1f88911
AnaKarolCampos <camposanakarol@gmail.com>
2025-01-11 09:13:06 -0300

    excluíndo "produtos" desnecessários

--- administracao/admin.py
+++ administracao/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from.models import Produtos,Cliente,Vendas
+from.models import Cliente,Vendas
 
-admin.site.register (Produtos)
 admin.site.register (Cliente)
 admin.site.register (Vendas)
--- administracao/models.py
+++ administracao/models.py
@@ -1,16 +1,5 @@
 from django.db import models
 
-class Produtos(models.Model):
-    nome = models.CharField(max_length=255)
-    descricao = models.TextField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    estoque = models.PositiveIntegerField()
-    categoria = models.CharField(max_length=100)
-
-    def str(self):
-        return self.nome
-
-
 class Cliente(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
