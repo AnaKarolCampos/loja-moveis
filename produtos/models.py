5db79f8c53c3b63a91ab56c4b8c8e00ba3e12f4b
AnaKarolCampos <camposanakarol@gmail.com>
2025-01-22 14:35:18 -0300

    finalizando calculo automático do valor da venda

--- produtos/models.py
+++ produtos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
@@ -21,33 +23,20 @@ class Catalogo(models.Model):
     def __str__(self):
         return self.nome
     
-'''class Venda(models.Model):
-    codigo = models.CharField(max_length=5)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    data_venda = models.DateField()
-    produtos = models.ManyToManyField(Produto)
-
-    def __str__(self):
-        return f"Venda {self.codigo} - {self.data_venda}"
-'''
 class Venda(models.Model):
     codigo = models.CharField(max_length=5)
-    data_venda = models.DateField(auto_now_add=True)
+    data = models.DateField(auto_now_add=True)
     produtos = models.ManyToManyField(Produto)
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-    def calcular_total(self):
-        return sum(produto.preco for produto in self.produtos.all())
-
-    def save(self, *args, **kwargs):
-        """ Salva a venda primeiro para gerar o ID, depois adiciona os produtos e recalcula o total """
-        super().save(*args, **kwargs)  # Salva a venda para gerar o ID
-        self.total = self.calcular_total()  # Agora que a venda tem ID, calcula o total
-        super().save(*args, **kwargs)  # Salva novamente com o total atualizado
-
     def __str__(self):
-        return f"Venda {self.codigo} - {self.data_venda} - Total: R${self.total}"
+        return f"Venda {self.codigo} - {self.data} - Total: R${self.total}"
 
-# Venda tem que ter o campo total, que tem que ser calculado na hora que a venda for salva
-# categoria de produtos tem que ser uma lista para escolher
+# Sinal para atualizar o total sempre que produtos forem adicionados/removidos
+@receiver(m2m_changed, sender=Venda.produtos.through)
+def atualizar_total_venda(sender, instance, action, **kwargs):
+    if action in ["post_add", "post_remove", "post_clear"]:  # Após adicionar/remover produtos
+        instance.total = sum(produto.preco for produto in instance.produtos.all())
+        instance.save()
+
 # cliente tem que ser linkado na venda, desce o model de cliente para a app produtos e linka no model de vendas com campo tipo ForeingKey
