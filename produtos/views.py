0b539d056ad98bbed4b6988db0dc7f0460621139
AnaKarolCampos <camposanakarol@gmail.com>
2025-02-01 09:53:58 -0300

    adicionando um catálogo na página principal

--- produtos/views.py
+++ produtos/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Catalogo
 
-def catalogo(request):
+'''def catalogo(request):
     catalogos = Catalogo.objects.filter(ativo=True)
+    return render(request, 'catalogo.html', { 'catalogos': catalogos })'''
 
-    return render(request, 'catalogo.html', { 'catalogos': catalogos })
+def catalogo(request):
+    catalogo_destaque = Catalogo.objects.filter(nome="Principais Ofertas!", ativo=True).first()
+    catalogos = Catalogo.objects.filter(ativo=True).exclude(id=catalogo_destaque.id) if catalogo_destaque else Catalogo.objects.filter(ativo=True)
+
+    return render(request, 'catalogo.html', {'catalogos': catalogos})
--- website/views.py
+++ website/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+'''from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html')'''
+
+from django.shortcuts import render
+from produtos.models import Catalogo  
+
+def home(request):
+    catalogo_destaque = Catalogo.objects.filter(nome="Principais Ofertas!").first()
+    return render(request, 'home.html', {'catalogo_destaque': catalogo_destaque})
